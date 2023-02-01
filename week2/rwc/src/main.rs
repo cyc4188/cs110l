97654320f1e8b2b9fc703dbde9fc734cd6d95100
cyc4188 <1065768794@qq.com>
2023-02-01 22:00:29 +0800

    word count

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +10,9 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let mut f = File::open(filename).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    println!("{} has {} words", filename, contents.split_whitespace().count());
 }
+
