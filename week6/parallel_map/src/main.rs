5a4e8c633e0680ae0cd8a857b24c1b73f184f69d
cyc4188 <1065768794@qq.com>
2023-02-23 00:23:14 +0800

    week6: parallel map

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,40 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+    let (in_sender, in_receiver) = crossbeam_channel::unbounded();
+    let (out_sender, out_receiver) = crossbeam_channel::unbounded();
+
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let in_receiver = in_receiver.clone();
+        let out_sender = out_sender.clone();
+        let thread = thread::spawn(move || {
+            while let Ok(input) = in_receiver.recv() {
+                let (idx, input) = input;
+                let output = f(input);
+                out_sender.send((idx, output)).unwrap();
+            }
+        });
+        threads.push(thread);
+    }
+
+    for (idx, input) in input_vec.drain(..).enumerate() {
+        in_sender.send((idx, input)).unwrap();
+    }
+
+    drop(in_sender);
+    drop(out_sender);
+
+    while let Ok((idx, output)) = out_receiver.recv() {
+        output_vec[idx] = output;
+    }
+
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
     output_vec
 }
 
